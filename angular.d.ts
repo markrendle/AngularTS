19db3a21bae9ded34028c673963fd935bc98fc3d
Greg Weber <greg@gregweber.info>
2012-10-15 06:45:30 -0700

    angular.module definition now works with my usage

--- angular.d.ts
+++ angular.d.ts
@@ -1,41 +1,42 @@
-module angular {
-    export function module(): Module;
-    export function module(name: string): Module;
-    export function module(name: string, requires: string[]): Module;
-    export function module(name: string, requires: string[], configFn: Function): Module;
-    export function module(name: string, configFn: Function): Module;
+interface angular {
+    module(): Module;
+    module(name: String): Module;
+    module(name: String, requires: string[]): Module;
+    // module(name: String, requires: string[], definition: Function): Module;
+    module(name: String, requires: string[], definition: any): Module;
+    module(name: String, configFn: Function): Module;
 }
 
-class Module {
+interface Module {
     config(configFn: Function);
     config(configs: any[]);
 }
 
 interface Scope {
     $apply(exp: Function): any;
-    $apply(exp: string): any;
+    $apply(exp: String): any;
     $destroy();
     $digest();
     $eval(exp: Function): any;
-    $eval(exp: string): any;
+    $eval(exp: String): any;
     $evalAsync(exp: Function): any;
-    $evalAsync(exp: string): any;
+    $evalAsync(exp: String): any;
 }
 
 interface HttpRequest {
-    method?: string;
-    url?: string;
+    method?: String;
+    url?: String;
     params?: any;
     data?: any;
     headers?: Object;
     transformRequest?: any;
     transformResponse?: any;
     cache?: any;
-    timeout?: number;
+    timeout?: Number;
     withCredentials?: bool;
 }
 
-interface HttpResponse { data: any; status: number; headers: Function; config: any; }
+interface HttpResponse { data: any; status: Number; headers: Function; config: any; }
 
 interface Promise {
     then(successCallback: (result) => any, errorCallback?: (reason) => any): Promise;
@@ -44,17 +45,17 @@ interface Promise {
 interface HttpPromise {
     then(successCallback: (response: HttpResponse) => any, errorCallback?: (response: HttpResponse) => any): Promise;
     success(fn: (data: any) => any): HttpPromise;
-    success(fn: (data: any, status: number) => any): HttpPromise;
-    success(fn: (data: any, status: number, headers: () => any, config?: any) => any): HttpPromise;
-    success(fn: (data: any, status: number, headers: (key: string) => string, config?: any) => any): HttpPromise;
+    success(fn: (data: any, status: Number) => any): HttpPromise;
+    success(fn: (data: any, status: Number, headers: () => any, config?: any) => any): HttpPromise;
+    success(fn: (data: any, status: Number, headers: (key: String) => string, config?: any) => any): HttpPromise;
     error(fn: (data: any) => any): HttpPromise;
-    error(fn: (data: any, status: number) => any): HttpPromise;
-    error(fn: (data: any, status: number, headers: () => any, config?: any) => any): HttpPromise;
-    error(fn: (data: any, status: number, headers: (key: string) => string, config?: any) => any): HttpPromise;
+    error(fn: (data: any, status: Number) => any): HttpPromise;
+    error(fn: (data: any, status: Number, headers: () => any, config?: any) => any): HttpPromise;
+    error(fn: (data: any, status: Number, headers: (key: String) => string, config?: any) => any): HttpPromise;
 }
 
 interface HttpService {
     (request: HttpRequest): HttpPromise;
-    get(url: string): HttpPromise;
-    get(url: string, config: HttpRequest): HttpPromise;
+    get(url: String): HttpPromise;
+    get(url: String, config: HttpRequest): HttpPromise;
 }
